92334b835bd3aa1b501fed7e41e3b5c27f2e50a4
Lara <lara.ej@gmail.com>
2018-07-26 19:32:44 +0100

    Adding test for names

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have names', function() {
+            for (var feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+        });
     });
 
 
